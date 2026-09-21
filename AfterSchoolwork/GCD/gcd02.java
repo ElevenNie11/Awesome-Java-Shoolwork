823dedcd4eadb318b5bad43ce2ba0c54c7c9a9ec
Eleven <nieshiyi20060927@qq.com>
2026-09-21 12:59:46 +0800

    for循环

---
+++ AfterSchoolwork/GCD/gcd02.java
@@ -0,0 +1,22 @@
+package AfterSchoolwork.GCD;
+import java.util.Scanner;
+
+public class gcd02 {
+    public static int GCD(int a, int b){
+        int gcd = 1;
+        for(int i = 1; i <= a && i <= b; i++){
+            if(a % i == 0 && b % i == 0){
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        int gcd = GCD(a, b);
+        System.out.print("最大公约数：" + gcd);
+    }
+}
